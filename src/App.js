e18bd280296f5d62eea3c076cd3f3b8468897fdf
vpdacoder <vaipatel@utexas.edu>
2018-05-02 13:01:37 -0500

    Completed Boilerplate for React Routing

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from './Home';
+import Procedures from './Procedures';
+import Contact from './Contact';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Go to Home Page</Link>{' '}
+            <Link to="/procedures">See Our Procedures</Link>{' '}
+            <Link to="/contact">Contact Us!</Link>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route path="/procedures" component={Procedures} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dentist Website</h1>
+        <p>
+          Welcome to my dentist website.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
